51e503198a238c548e6cf4f85acdc4e56cd6f242
totymedli <totymedli@keresztech.org>
2025-07-14 06:36:23 +0200

    fix github ci error

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '',
   plugins: [react()],
+  worker: {
+    format: 'es',
+    rollupOptions: {
+      external: ['./src/pages/sharedWorker.js'],
+    },
+  },
 })
